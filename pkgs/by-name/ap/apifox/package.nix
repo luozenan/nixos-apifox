4a048b69f94b47337ac90cb87a7e89e6ccd24275
zenan.luo <zenan.luo@cue.group>
2026-02-09 15:22:48 +0800

    2.8.2 -> 2.8.5

--- pkgs/by-name/ap/apifox/package.nix
+++ pkgs/by-name/ap/apifox/package.nix
@@ -3,7 +3,7 @@
 
 let
   pname = "apifox";
-  version = "2.8.2";
+  version = "2.8.5";
   # 1. 下载包含AppImage的zip包
   zipSrc = pkgs.fetchzip {
     url = "https://file-assets.apifox.com/download/Apifox-linux-latest.zip";
