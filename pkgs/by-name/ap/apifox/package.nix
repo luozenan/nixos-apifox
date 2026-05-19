44320be40422b7a48ae773b469c28047643787d9
zenan.luo <zenan.luo@cue.group>
2026-05-19 15:34:11 +0800

    update hash

--- pkgs/by-name/ap/apifox/package.nix
+++ pkgs/by-name/ap/apifox/package.nix
@@ -7,7 +7,7 @@ let
   # 1. 下载包含AppImage的zip包
   zipSrc = pkgs.fetchzip {
     url = "https://file-assets.apifox.com/download/Apifox-linux-latest.zip";
-    sha256 = "sha256-7AnC55MYRj0SF2eWblIB9ULH8bxvM2LqqIPi0/Mdk1g="; # 替换为实际hash
+    sha256 = "sha256-LgciNKw7FMrpFO4AI1pRi8uTpjvVyOlZrDYxxM/h4Z0="; # 替换为实际hash
     stripRoot = false; # 保留zip内的原始目录
   };
 
